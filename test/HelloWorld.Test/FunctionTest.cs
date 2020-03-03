249ce9ac7b0771a7d50613300673f5f673aa15c7
cp <chris@cpsharp.net>
2020-03-03 00:20:59 -0500

    first commit!

---
+++ test/HelloWorld.Test/FunctionTest.cs
@@ -0,0 +1,64 @@
+namespace HelloWorld.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using System.Net.Http;
+    using Newtonsoft.Json;
+    using Xunit;
+    using Amazon.Lambda.TestUtilities;
+    using Amazon.Lambda.APIGatewayEvents;
+    using Xunit.Abstractions;
+
+    public class FunctionTest
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+        private static readonly HttpClient client = new HttpClient();
+
+        public FunctionTest(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+        private static async Task<string> GetCallingIP()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Add("User-Agent", "AWS Lambda .Net Client");
+
+            var stringTask = client.GetStringAsync("http://checkip.amazonaws.com/").ConfigureAwait(continueOnCapturedContext: false);
+
+            var msg = await stringTask;
+            return msg.Replace("\n", "");
+        }
+
+        [Fact]
+        public async Task TestHelloWorldFunctionHandler()
+        {
+            var request = new APIGatewayProxyRequest();
+            var context = new TestLambdaContext();
+            string location = GetCallingIP().Result;
+            Dictionary<string, string> body = new Dictionary<string, string>
+            {
+                {"message", "hello world"},
+                {"location", location},
+            };
+
+            var expectedResponse = new APIGatewayProxyResponse
+            {
+                Body = JsonConvert.SerializeObject(body),
+                StatusCode = 200,
+                Headers = new Dictionary<string, string> {{"Content-Type", "application/json"}}
+            };
+
+            var function = new Function();
+            var response = await function.FunctionHandler(request, context);
+
+            testOutputHelper.WriteLine("Lambda Response: \n" + response.Body);
+            testOutputHelper.WriteLine("Expected Response: \n" + expectedResponse.Body);
+
+            Assert.Equal(expectedResponse.Body, response.Body);
+            Assert.Equal(expectedResponse.Headers, response.Headers);
+            Assert.Equal(expectedResponse.StatusCode, response.StatusCode);
+        }
+    }
+}
